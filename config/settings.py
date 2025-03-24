344e69fd73e32766374939135974b6bcba45a351
Ajishi Singh <ajishisingh2@gmail.com>
2025-03-25 01:38:29 +0530

    Updated code with latest changes

--- config/settings.py
+++ config/settings.py
@@ -66,8 +66,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME', 'gas_utility_db'),
-        'USER': os.getenv('DB_USER', 'ajishisingh'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'ajishi'),
+        'USER': os.getenv('DB_USER', ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '3306'),
     }
